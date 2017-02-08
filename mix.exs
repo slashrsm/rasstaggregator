212158aa39aae232fa434632ad2712e4bdabe156
Janez Urevc <janez@janezurevc.name>
2017-02-08 11:03:37 +0100

    Add package information.

--- mix.exs
+++ mix.exs
@@ -11,26 +11,34 @@ defmodule RaSStaggregator.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     source_url: "https://github.com/slashrsm/rasstaggregator",
+     description: "Feed aggregator for Elixir.",
+     package: package(),
      deps: deps()]
   end
 
   def application do
-    [extra_applications: [:logger],
-    mod: {RaSStaggregator, []}]
+    [
+      extra_applications: [:logger, :httpoison],
+      mod: {RaSStaggregator, []}
+    ]
   end
 
   defp deps do
     [
       {:feeder_ex, "~> 1.0"}, 
       {:httpoison, "~> 0.11.0"}, 
-      {:timex, "~> 3.0"}, 
+      #{:timex, "~> 3.0"},
       {:dialyze, only: [:dev, :test]}
     ]
   end
 
   defp package do
-    [ maintainers: ["Janez Urevc"],
-      licenses: ["GPL 2"],
-      links: %{"Github" => "https://github.com/slashrsm/rasstaggregator"} ]
+    [ 
+      files: ["lib", "mix.exs", "README.md"],
+      maintainers: ["Janez Urevc"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/slashrsm/rasstaggregator"}
+    ]
   end
 end
